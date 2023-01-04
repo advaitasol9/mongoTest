0d029410334b459072b49ae85250f9a63e0c04e1
advaitasol9 <advaitasol9@gmail.com>
2023-01-04 16:35:38 +0530

    simplified

--- index.js
+++ index.js
@@ -11,23 +11,24 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri);
-const connection = mongoose.connection;
-connection.once("open", () => console.log("Database connected!"));
+// const uri = process.env.ATLAS_URI;
+// mongoose.connect(uri);
+// const connection = mongoose.connection;
+// connection.once("open", () => console.log("Database connected!"));
 
 const exerciseRouter = require("./routes/exercises");
 const userRouter = require("./routes/users");
 
 app.get("/", (req, res) => {
-  var dataToSend;
-  const python = spawn("python", ["./pythonScript/lineupGen.py", "1"]);
-  python.stdout.on("data", function (data) {
-    dataToSend = data.toString();
-  });
-  python.on("close", (code) => {
-    res.send(dataToSend);
-  });
+  // var dataToSend;
+  // const python = spawn("python", ["./pythonScript/lineupGen.py", "1"]);
+  // python.stdout.on("data", function (data) {
+  //   dataToSend = data.toString();
+  // });
+  // python.on("close", (code) => {
+  //   res.send(dataToSend);
+  // });
+  res.send("connected");
 });
 
 app.use("/exercises", exerciseRouter);
