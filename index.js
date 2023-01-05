2ef019c51d30dea78a17cb9439876d19152a0e00
advaitasol9 <advaitasol9@gmail.com>
2023-01-05 10:14:27 +0530

    wtf

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// const uri = process.env.ATLAS_URI;
+// const uri = process.env.ATLAS_URI || "ATLAS_URI = "mongodb+srv://brijesh:brijesh97s@cluster0.aaak4ma.mongodb.net/?retryWrites=true&w=majority"";
 // mongoose.connect(uri);
 // const connection = mongoose.connection;
 // connection.once("open", () => console.log("Database connected!"));
